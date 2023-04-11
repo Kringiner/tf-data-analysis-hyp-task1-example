53272ea6edc663da60a3efc7ae091886ed03b4b8
Alexander <55991290+Kringiner@users.noreply.github.com>
2023-04-11 22:45:54 +0500

    attemp 1

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
-import pandas as pd
 import numpy as np
+from scipy.stats import norm
+
+chat_id = 843200348
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p = 0.05
+    norm_ppf = norm.ppf(p)
+    diff_p = (x_success + y_success) / (x_cnt + y_cnt)
+    return (y_success / y_cnt - x_success / x_cnt) / np.sqrt(
+        diff_p * (1 - diff_p) / x_cnt + diff_p * (1 - diff_p) / y_cnt) < norm_ppf
